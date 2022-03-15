9f33270a5f3a714b830dc6b03e1dccbe84d25541
Vinicius Vellozo <vinicius.vellozo@hotmail.com>
2022-03-14 22:37:22 -0300

    feature: race map graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Color } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-root',
@@ -41,6 +42,80 @@ export class AppComponent {
   @Output() stintsChange = new EventEmitter<Tyre[]>();
   stratTotalLaps: number = 0;
 
+  multi = [
+    {
+      name: 'Soft',
+      series: [
+        {
+          name: '0',
+          value: 7,
+        },
+        {
+          name: '10',
+          value: 0,
+        },
+      ],
+    },
+    {
+      name: 'Hard',
+      series: [
+        {
+          name: '10', //startlap
+          value: 15, //life lap
+        },
+        {
+          name: '15', //end lap
+          value: 0, // end life
+        },
+      ],
+    },
+    {
+      name: 'Medium',
+      series: [
+        {
+          name: '15',
+          value: 10,
+        },
+        {
+          name: '25',
+          value: 0,
+        },
+      ],
+    },
+  ];
+  view: [number, number] = [880, 320];
+  legend: boolean = false;
+  showLabels: boolean = false;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Lap';
+  yAxisLabel: string = 'Tyre Life';
+  timeline: boolean = false;
+  colorScheme: any[] = [];
+
+  constructor() {
+    this.stints.push(
+      {
+        compound: this.tyres[2].compound,
+        laps: this.tyres[2].laps,
+        threshold: this.tyres[2].threshold,
+      },
+      {
+        compound: this.tyres[1].compound,
+        laps: this.tyres[1].laps,
+        threshold: this.tyres[1].threshold,
+      },
+      {
+        compound: this.tyres[0].compound,
+        laps: this.tyres[0].laps,
+        threshold: this.tyres[0].threshold,
+      }
+    );
+    this.calcStratLaps();
+  }
 
   changeLaps(value: any) {
     this.totalLaps = value.target.value;
@@ -97,13 +172,48 @@ export class AppComponent {
       console.log(`S:[${i}], s.laps:${s.laps}`);
       this.stratTotalLaps += s.laps;
     });
+    this.drawChart();
   }
   calcPitLap(stints: Tyre[], n: number) {
     let pitLap = 0;
-    for (let i = 0; i <= n; i++) {
-      pitLap += stints[i].laps;
+    for (let i = 1; i <= n; i++) {
+      pitLap += stints[i-1].laps;
     }
     return pitLap;
+  }
+
+  drawChart() {
+    let map: any[] = [];
+    let tyreColors = {
+      soft: '#ef4444',
+      medium: '#facc15',
+      hard: '#fff',
+      intermediate: '#22c55d',
+      wet: '#3683f6',
+    };
+    let colors: any[] = [];
+    let currentLap = 0;
+    this.stints.forEach((s, i) => {
+      colors.push({ name: s.compound, value: tyreColors[s.compound] });
+      map.push({
+        name: s.compound,
+        series: [
+          {
+            name: currentLap, //start lap
+            value: s.laps, //life lap
+          },
+          {
+            name: currentLap + s.laps, //end lap
+            value: 0, // end life
+          },
+        ],
+      });
+      currentLap += s.laps;
+    });
+    this.multi = map;
+    this.colorScheme = colors;
+    console.log('map:', this.multi);
+    console.log('color:', this.colorScheme);
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxChartsModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
